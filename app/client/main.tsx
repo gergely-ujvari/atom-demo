616b12526ec34e01e438690ec447b77bb831f3e7
Gergely Ujvari <ujvari@nolmecolindor.com>
2016-10-12 10:34:20 +0200

    Initial commit

---
+++ app/client/main.tsx
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import * as React from 'react';
+import { render } from 'react-dom';
+import { MainApp } from './ui/MainApp';
+
+Meteor.startup(() => {
+  render(<MainApp/>, document.getElementById('render-target'))
+});
+
---
+++ app/client/ui/MainApp.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class MainApp extends React.Component<{},{}> {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
