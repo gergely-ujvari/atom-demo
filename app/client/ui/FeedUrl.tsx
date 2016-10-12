e969999ad991ddacb385c8744ee109b9a0b73fbe
Gergely Ujvari <ujvari@nolmecolindor.com>
2016-10-12 19:50:12 +0200

    React to Enter key in the input URL

--- app/client/ui/FeedUrl.tsx
+++ app/client/ui/FeedUrl.tsx
@@ -8,18 +8,30 @@ export interface FeedUrlProps {
 
 // Simple form for dropping the feed URL.
 export class FeedUrl extends React.Component<FeedUrlProps, {}> {
-    onButtonClick (event:Event) {
-        event.preventDefault();
+    private submit () {
         const url = ReactDOM.findDOMNode<HTMLInputElement>(this.refs['feedUrl']).value.trim();
         this.props.setFeedUrl(url);
     }
 
-    render () {
+    onButtonClick (event:Event) {
+        event.preventDefault();
+        this.submit();
+    }
+
+    // React to Enter button
+    onInputKeyPress (event:KeyboardEvent) {
+        if (event.key === 'Enter') {
+            this.submit();
+        }
+    }
+
+    public render () {
         return (
             <div className="row">
                 <div className="input-group">
                     <input className="form-control"
                            defaultValue={this.props.url}
+                           onKeyPress={this.onInputKeyPress.bind(this)}
                            placeholder="Feed URL"
                            ref="feedUrl"
                            type="url"
